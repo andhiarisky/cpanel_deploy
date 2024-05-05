ae6f24e89d9d497de3e44a243ad29aacc063ae10
andhiarisky <andhi@rumahweb.co.id>
2024-05-05 17:06:53 +0700

    Update vps-standard.sh

--- vps-standard.sh
+++ vps-standard.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
-#download-git
-wget 
+#download-git-cpanel-deploy
+wget https://github.com/andhiarisky/cpanel_deploy/archive/refs/heads/main.zip -O /usr/local/src/cpanel_deploy.zip
+cd /usr/local/src/ && unzip cpanel_deploy.zip
+
 #recompile-easyapache4
 mkdir -p /etc/cpanel/ea4/profiles/custom/
-wget https://github.com/andhiarisky/cpanel_deploy/blob/main/ea-php72-php83.json -O /etc/cpanel/ea4/profiles/custom/eaphp72-83cl.json
-/usr/local/bin/ea_install_profile --install /etc/cpanel/ea4/profiles/custom/eaphp70-80cl.json
+mv /usr/local/src/ea-php72-php83.json /etc/cpanel/ea4/profiles/custom/ea-php72-php83.json
+/usr/local/bin/ea_install_profile --install /etc/cpanel/ea4/profiles/custom/ea-php72-php83.json
 
 #install-modsec360
-/usr/local/cpanel/scripts/modsec_vendor add https://files.imunify360.com/static/modsec/v1/meta_imunify360_min.yaml
+/usr/local/cpanel/scripts/modsec_vendor add https://files.imunify360.com/static/modsec/v2/meta_imunify360-full-apache.yaml
+
+#install csf, cmc, cmq
+cd /usr/local/src/;wget https://download.configserver.com/csf.tgz;tar -zxvf csf.tgz;cd csf;./install.cpanel.sh;cd /usr/local/src/;wget https://download.configserver.com/cmq.tgz;tar -zxvf cmq.tgz;cd  cmq;./install.sh;cd /usr/local/src/;wget https://download.configserver.com/cmc.tgz;tar -zxvf cmc.tgz;cd  cmc;./install.sh
 
 #copy-csf-conf
-wget http://103.247.10.16/csf -O /etc/csf/csf.conf
+mv /usr/local/src/csf.conf /etc/csf/csf.conf
 csf -r
 
 #copy-tweak-setting
-wget http://103.247.10.16/cpanel -O /var/cpanel/cpanel.config
+mv /usr/local/src/cpanel.config /var/cpanel/cpanel.config
 whmapi1 restore_config_from_file module=Main path=/var/cpanel/cpanel.config
 
 #ubah-handler-lsapi
-/usr/local/cpanel/bin/rebuild_phpconf --ea-php80=lsapi
-/usr/local/cpanel/bin/rebuild_phpconf --ea-php74=lsapi
 /usr/local/cpanel/bin/rebuild_phpconf --ea-php73=lsapi
-/usr/local/cpanel/bin/rebuild_phpconf --ea-php72=lsapi
-/usr/local/cpanel/bin/rebuild_phpconf --ea-php71=lsapi
-/usr/local/cpanel/bin/rebuild_phpconf --ea-php70=lsapi
+/usr/local/cpanel/bin/rebuild_phpconf --ea-php74=lsapi
+/usr/local/cpanel/bin/rebuild_phpconf --ea-php80=lsapi
+/usr/local/cpanel/bin/rebuild_phpconf --ea-php81=lsapi
+/usr/local/cpanel/bin/rebuild_phpconf --ea-php82=lsapi
+/usr/local/cpanel/bin/rebuild_phpconf --ea-php83=lsapi
+
+#update php.ini
+mv /usr/local/src/ea-php73.ini /opt/cpanel/ea-php73/root/etc/php.ini
+mv /usr/local/src/ea-php74.ini /opt/cpanel/ea-php74/root/etc/php.ini
+mv /usr/local/src/ea-php80.ini /opt/cpanel/ea-php80/root/etc/php.ini
+mv /usr/local/src/ea-php81.ini /opt/cpanel/ea-php81/root/etc/php.ini
+mv /usr/local/src/ea-php82.ini /opt/cpanel/ea-php82/root/etc/php.ini
+mv /usr/local/src/ea-php83.ini /opt/cpanel/ea-php83/root/etc/php.ini
